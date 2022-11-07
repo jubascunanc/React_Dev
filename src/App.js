277e4e2a23b5a50cd1516a7e46d0b19d9b2a284e
Juan Jose Bascuñan <jubascunanc@udd.cl>
2022-11-07 14:04:12 -0400

    React Host

--- src/App.js
+++ src/App.js
@@ -1,53 +1,13 @@
-import logo from './imagen/logo-emprende.png';
-import './App.css';
-import Banner from './imagen/clases-online.jpg'
-import './Componentes/Nabvar/Navbar';
-import './Componentes/Nabvar/Navbar.scss';
-import Card from './Componentes/Item/Item';
-import Bienvenida from './Componentes/Elementos/Bienvenida';
-import Boton from './Componentes/Elementos/Boton';
+import { NavBar } from "./Componentes/Nabvar/Navbar";
+
 
 function App() {
 
-let bienvenida= 'Bienvenidos a Mprende App';
-let Inscribete= 'Registrate Gratuitamente con un 40% dscto en tu beca';
+
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-      <div className="Navbar">
-      <img src={logo} className="App-logo" alt="logo" />
-        <nav>
-          <h2>Inscribete aquí</h2>
-          <Boton prop={Inscribete}/>
-        </nav>
-        </div>
-        <div className="Banner">
-        <img src={Banner} />
-       </div>
-        <h3>
-          <button>
-          ¡Inicia Sesión Aquí!
-          </button>
-        </h3>
-        <h1>
-         <Bienvenida prop={bienvenida} />
-        </h1>
-        <h2>
-          Cursos Online con profesores expertos en la educación virtual 100% certificado
-        </h2>
-          <div className="content-app">
-          <p>
-            Inscribete a nuestra plataforma de cursos online en modalidad Remoto e hibrido, para 
-            aprender nuevas habilidades educativas y profesionales con otros alumnos en la comunidad.
-          </p>
-          </div>
-          <div className="Card-details">
-            <Card prop={Card}/>
-          </div>
-      </header>
-    </div>
+  <div><NavBar/></div>
   );
 }
 
---
+++ src/Componentes/Nabvar/Navbar.jsx
@@ -0,0 +1,59 @@
+import { NavLink } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import logo from './imagen/logo-emprende.png';
+import Banner from './imagen/clases-online.jpg'
+import './Componentes/Nabvar/Navbar';
+import './Componentes/Nabvar/Navbar.scss';
+import Card from './Componentes/Item/Item';
+import Bienvenida from './Componentes/Elementos/Bienvenida';
+import Boton from './Componentes/Elementos/Boton';
+import Item from './Componentes/Item/Item';
+
+
+function NavBar() {
+
+let bienvenida= 'Bienvenidos a Mprende App';
+let Inscribete= 'Registrate Gratuitamente con un 40% dscto en tu beca';
+
+
+  return (
+    <div className="App">
+    <header className="App-header">
+    <div className="Navbar">
+    <img src={logo} className="App-logo" alt="logo" />
+    
+      <nav>
+        <h2>Inscribete aquí</h2>
+        <Boton prop={Inscribete}/>
+      </nav>
+      </div>
+      <div className="Banner">
+      <img src={Banner} />
+     </div>
+      <h3>
+        <button>
+        ¡Inicia Sesión Aquí!
+        </button>
+      </h3>
+      <h1>
+       <Bienvenida prop={bienvenida} />
+      </h1>
+      <h2>
+        Cursos Online con profesores expertos en la educación virtual 100% certificado
+      </h2>
+        <div className="content-app">
+        <p>
+          Inscribete a nuestra plataforma de cursos online en modalidad Remoto e hibrido, para 
+          aprender nuevas habilidades educativas y profesionales con otros alumnos en la comunidad.
+        </p>
+        </div>
+        <div className="Card-details">
+          <Card prop={Item}/>
+        </div>
+    </header>
+  </div>
+  );
+}
+
+export default NavBar;
